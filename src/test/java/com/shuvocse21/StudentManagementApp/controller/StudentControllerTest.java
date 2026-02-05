df6cd14846d9b68ff0157fd9e166956f220fcc75
commoner02 <77.shuvo.joy@gmail.com>
2026-02-05 09:48:05 +0600

    ci file added

---
+++ src/test/java/com/shuvocse21/StudentManagementApp/controller/StudentControllerTest.java
@@ -0,0 +1,16 @@
+package com.shuvocse21.StudentManagementApp.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void profile() {
+    }
+
+    @Test
+    void updateProfile() {
+    }
+}
